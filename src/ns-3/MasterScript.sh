ba0d590f78b35c05fc349a4231fae9a3e1fe9291
renu285 <renu285@gmail.com>
2016-04-30 23:32:54 +0200

    minor changes to master script

--- src/ns-3/MasterScript.sh
+++ src/ns-3/MasterScript.sh
@@ -5,8 +5,17 @@ echo " Wireless Networking NS3 Simulation"
 echo " "
 echo "##########################################################"
 
-
 ./wifi-Script-CW-throughput.sh
 ./wifi-Script-CW-delay.sh  
 ./wifi-Script.sh
 ./wifi-Script-rate-BEB.sh 
+echo ""
+echo " -- The results of Simulation are present in "
+echo "Contentionwindowvsthroughput.png - comparision of contention window with throughput"
+echo "Contentionwindowvsdelay.png      - Comparision of contention window with delay"
+echo "Contentionwindowvsdelay.png      - Comparision of contention window with delay"
+echo "throughputvsNodes.png  - Throughput variation comparision"
+echo "packetlossvsNodes.png - Packet Loss percentage variation comparision"
+echo "ratevsthroughput.png  - Throughput variation comparision"
+echo "delayvsthroughput.png  - Throughput variation comparision"
+
